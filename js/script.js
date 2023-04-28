f468a647f61f5ae4739ca72d578da49f8ba598c4
Madie Few <mfew.business@gmail.com>
2023-04-28 13:06:08 -0700

    First commit attempt

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// the element i selected was the div w a class of "overview"
+const overview = document.querySelector(".overview");
+const username = "Madie-F";
+
+const gitUserInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> 
+    `;
+    overview.append(div);
+};
