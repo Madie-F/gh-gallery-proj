913c279f4dfe9e1282a93631a584ed4fdc18b346
Madie Few <mfew.business@gmail.com>
2023-05-03 10:39:12 -0700

    Create dynamic search

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const overview = document.querySelector(".overview");
 const username = "Madie-F";
 const repoList = document.querySelector(".repo-list");
 const allReposContainer = document.querySelector(".repos");
-const repoData = document.querySelector(".repo-data")
+const repoData = document.querySelector(".repo-data");
+const viewReposButton = document.querySelector(".view-repos");
+const filterInput = document.querySelector(".filter-repos");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -17,18 +19,18 @@ const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = `
-    <figure>
-      <img alt="user avatar" src=${data.avatar_url} />
-    </figure>
-    <div>
-      <p><strong>Name:</strong> ${data.name}</p>
-      <p><strong>Bio:</strong> ${data.bio}</p>
-      <p><strong>Location:</strong> ${data.location}</p>
-      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div> 
+        <figure>
+          <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+          <p><strong>Name:</strong> ${data.name}</p>
+          <p><strong>Bio:</strong> ${data.bio}</p>
+          <p><strong>Location:</strong> ${data.location}</p>
+          <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div> 
     `;
     overview.append(div);
-    gitRepos();
+    gitRepos(username);
 };
 
 const gitRepos = async function () {
@@ -38,6 +40,7 @@ const gitRepos = async function () {
 };
 
 const displayRepos = function (repos) {
+    filterInput.classList.remove("hide");
     for (const repo of repos){
         const repoItem = document.createElement("li");
         repoItem.classList.add("repo");
@@ -57,7 +60,7 @@ repoList.addEventListener("click", function (e) {
 const getRepoInfo = async function (repoName) {
     const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
     const repoInfo = await fetchInfo.json();
-    console.log(repoInfo);
+    // console.log(repoInfo);
     // Grab languages
     const fetchLanguages = await fetch(repoInfo.languages_url);
     const languageData = await fetchLanguages.json();
@@ -72,16 +75,38 @@ const getRepoInfo = async function (repoName) {
 };
 
 const displayRepoInfo = function (repoInfo, languages) {
+    viewReposButton.classList.remove("hide");
     repoData.innerHTML = "";
     repoData.classList.remove("hide");
     allReposContainer.classList.add("hide");
     const div = document.createElement("div");
     div.innerHTML = `
-    <h3>Name: ${repoInfo.name}</h3>
-    <p>Description: ${repoInfo.description}</p>
-    <p>Default Branch: ${repoInfo.default_branch}</p>
-    <p>Languages: ${languages.join(", ")}</p>
-    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+        <h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
     `;
     repoData.append(div);
 };
+
+viewReposButton.addEventListener("click", function () {
+    allReposContainer.classList.remove("hide");
+    repoData.classList.add("hide");
+    viewReposButton.classList.add("hide");
+});
+
+filterInput.addEventListener("click", function () {
+    const searchTest = e.target.value;
+    const repos = document.querySelectorAll(".repo");
+    const searchLowerText = searchText.toLowerCase();
+
+    for (const repo of repos) {
+        const repoLowerText = repo.innerText.toLowerCase();
+        if (repoLowerText.includes(searchLowerText)) {
+            repo.classList.remove("hide");
+        } else {
+            repo.classList.add("hide");
+        }
+    }
+});
